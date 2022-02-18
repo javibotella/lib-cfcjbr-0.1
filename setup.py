9fb3ceee8a13bb8ae92ef05b4dbe6da1dbeb1093
Javier Botella Rodriguez <jbotella.rodriguez@servinformgroup.local>
2022-02-18 09:33:41 +0100

    primera version

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+    name='lib-cfcjbr',
+    version='0.1',
+    description='Este es un paquete de jemplo',
+    author='Javier Botella',
+    author_email='jbotella.rodriguez@servinform.es',
+    # url='http://www.hektorprofe.net',
+    packages=['lib-cfcjbr','lib-cfcjbr.hola','lib-cfcjbr.adios'],
+    scripts=[]
+)
